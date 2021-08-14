23dd0695b90fd17efd45b2b7cbb5f1a3fb2d79e9
Tobel <tobelwubshet@gmail.com>
2021-08-14 00:37:52 +0000

    Ui form setup

--- pages/api/twitch.js
+++ pages/api/twitch.js
@@ -1,4 +1,27 @@
 // This is where all the logic for your Twitch API will live!
 export default async (req, res) => {
-  res.status(200).json({data: 'Welcome to Twitch API!'})
+  try {
+    if(req.method == "POST") {
+      const {data} = req.body
+      const accessToken = await getTwitchAccessToken()
+      console.log(accesToken)
+      res.status(200).json({data})
+    }
+
+  } catch(error) {
+    res.status(500).send()
+  }
+}
+
+//actions
+const getTwitchAccessToken = async () => {
+  const path = `https:/id/twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_SECRET_ID}&grant_type=client_credentials`
+
+  const response = await fetch(path, {
+    method: 'POST'
+  })
+  if(response) {
+    const json = await response.json()
+    return json.access_token
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,49 @@
 // Main entry point of your app
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Rect, {useState} from 'react'
 
 const Home = () => {
+  //useState
+  const [favoriteChannel, setFavoriteChannel] = useState([])
+
+  const addChannel = async event => {
+    event.preventDefault() //prevents default behavior of page reloading on form submit
+    const {value} = event.target.elements.name
+    if(value) {
+      //call twitch api
+      const path = `https://${window.location.hostname}`
+      const response = await fetch(`${path}/api/twitch`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({data: value})
+      })
+
+      const json = await response.json()
+      console.log("From server: ", json.data)
+
+
+      setFavoriteChannel(prevState => [...prevState, value])
+      event.target.elements.name.value = ""
+    }
+    console.log(value)
+
+  }
+
+
+  const renderForm = () => {
+    return (<div className={styles.formContainer}> 
+      <form onSubmit={addChannel}>
+        <input id="name" placeholder="Twitch Channel Name" type="text" required />
+        <button type="submit">Add Streamer</button>
+      </form>
+    </div>)
+  }
+
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,7 +51,7 @@ const Home = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className={styles.inputContainer}>
-        <h1>Welcome to the Personalized Twitch Dashboard! 🎉</h1>
+       {renderForm()}
       </div>
     </div>
   )
